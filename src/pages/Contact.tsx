94a2079a1549d883ce167b81f3f715dd840a0415
Tomasz Hofman <tomasz.k.hofman@gmail.com>
2022-05-31 08:42:29 +0200

    feat: add pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InputForm } from "../components/InputForm";
+
+const Contact = () => {
+  return (
+    <div>
+      <InputForm />
+    </div>
+  );
+};
+
+export default Contact;
